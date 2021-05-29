d63366b84db95756667aa748705cd3431cb44f6e
suraj4VComply <suraj@v-comply.com>
2021-05-29 18:57:11 +0530

    checkbox

--- projects/gorilla-ui/src/lib/components/checkbox/checkbox/checkbox/checkbox.component.ts
+++ projects/gorilla-ui/src/lib/components/checkbox/checkbox/checkbox/checkbox.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'gui-checkbox',
   templateUrl: './checkbox.component.html',
-  styleUrls: ['./checkbox.component.css']
+  styleUrls: ['./checkbox.component.less']
 })
 export class CheckboxComponent implements OnInit {
 
